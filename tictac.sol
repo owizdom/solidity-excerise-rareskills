7cd563811dc876b3775ac9d78ecd1c1b6d8925cb
Wisdom <154702154+clowwnFace@users.noreply.github.com>
2024-04-08 17:25:26 +0100

    Add files via upload

---
+++ tictac.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract TicTacToe {
+    /* 
+        This exercise assumes you know how to manipulate nested array.
+        1. This contract checks if TicTacToe board is winning or not.
+        2. Write your code in `isWinning` function that returns true if a board is winning
+           or false if not.
+        3. Board contains 1's and 0's elements and it is also a 3x3 nested array.
+    */
+
+    function isWinning(uint8[3][3] memory board) public pure returns (bool) {
+        // Check rows
+        for (uint8 i = 0; i < 3; i++) {
+            if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true; // Winning row
+            }
+        }
+        
+        // Check columns
+        for (uint8 j = 0; j < 3; j++) {
+            if (board[0][j] != 0 && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
+                return true; // Winning column
+            }
+        }
+        
+        // Check diagonals
+        if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true; // Winning diagonal (top-left to bottom-right)
+        }
+        if (board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true; // Winning diagonal (top-right to bottom-left)
+        }
+        
+        // No winning combination found
+        return false;
+    }
+}
